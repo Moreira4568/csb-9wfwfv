7e8bb2fed5512852eeb112761a43246f2f1caff9
Code Sandbox <hello@codesandbox.io>
2022-08-09 12:21:08 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b3332e3342909da01b8b0ae3e932fcbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd543cc02b1ded0f8faa127e03a53d1",
+    "url": "https://moreira4568.github.io/csb-9wfwfv/index.html"
+  },
+  {
+    "revision": "2286dd131c51ace7cfe4",
+    "url": "https://moreira4568.github.io/csb-9wfwfv/static/css/main.c2858c0b.chunk.css"
+  },
+  {
+    "revision": "11fb1cfc4a0b60ed6302",
+    "url": "https://moreira4568.github.io/csb-9wfwfv/static/js/2.cc33dbb2.chunk.js"
+  },
+  {
+    "revision": "3b4b0ea519b304fdf1730bf1fc46732f",
+    "url": "https://moreira4568.github.io/csb-9wfwfv/static/js/2.cc33dbb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2286dd131c51ace7cfe4",
+    "url": "https://moreira4568.github.io/csb-9wfwfv/static/js/main.3fb33e77.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "https://moreira4568.github.io/csb-9wfwfv/static/js/main.3fb33e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "983a07bf4d944b59cb92",
+    "url": "https://moreira4568.github.io/csb-9wfwfv/static/js/runtime-main.89a8f1fe.js"
+  }
+]);
